98ba0bec1d05a0719de0bc2eba18d5ec20dbbf0f
HDieTox <tom.compagnon2003@gmail.com>
2025-05-06 13:12:16 +0200

    Debug 3.0

--- rover_control/src/ppm_converter.cpp
+++ rover_control/src/ppm_converter.cpp
@@ -2,7 +2,7 @@
 #include "std_msgs/msg/int16_multi_array.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
-#include <serial/serial.h>
+#include <serial.h>
 
 class PPMConverter : public rclcpp::Node {
 public:
