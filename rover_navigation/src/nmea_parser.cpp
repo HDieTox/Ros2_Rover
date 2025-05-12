e83c298e9031e3fa64c1439fa5d5ab966401394d
HDieTox <tom.compagnon2003@gmail.com>
2025-05-12 14:08:04 +0200

    Debug 4.13

--- rover_navigation/src/nmea_parser.cpp
+++ rover_navigation/src/nmea_parser.cpp
@@ -2,7 +2,7 @@
 #include "nmea_msgs/msg/sentence.hpp"
 #include "sensor_msgs/msg/nav_sat_fix.hpp"
 #include <nmea.h>
-#include <nmea/gpgga.h> 
+#include <nmea/gpgga.h>
 
 class NMEAParser : public rclcpp::Node
 {
@@ -16,40 +16,47 @@ public:
     }
 
 private:
-void nmea_callback(const nmea_msgs::msg::Sentence::SharedPtr msg)
-{
-    // Copier la chaîne NMEA dans un buffer modifiable
-    std::string sentence = msg->sentence;
-    char *buffer = &sentence[0];  // chaîne modifiable
+    void nmea_callback(const nmea_msgs::msg::Sentence::SharedPtr msg)
+    {
+        // Copier la chaîne NMEA dans un buffer modifiable
+        std::string sentence = msg->sentence;
+        char *buffer = &sentence[0]; // chaîne modifiable
 
-    // Parser la phrase NMEA
-    nmea_s *data = nmea_parse(buffer, sentence.size(), 0);
-    if (data == nullptr) {
-        RCLCPP_WARN(this->get_logger(), "Failed to parse NMEA sentence");
-        return;
-    }
+        // Parser la phrase NMEA
+        nmea_s *data = nmea_parse(buffer, sentence.size(), 0);
+        if (data == nullptr)
+        {
+            RCLCPP_WARN(this->get_logger(), "Failed to parse NMEA sentence");
+            return;
+        }
 
-    if (data->type == NMEA_GPGGA) {
-        // Caster vers le type spécifique
-        nmea_gpgga_s *gpgga = (nmea_gpgga_s *)data;
+        if (data->type == NMEA_GPGGA)
+        {
+            // Caster vers le type spécifique
+            nmea_gpgga_s *gpgga = (nmea_gpgga_s *)data;
 
-        auto fix = sensor_msgs::msg::NavSatFix();
-        fix.header.stamp = this->get_clock()->now();
+            auto fix = sensor_msgs::msg::NavSatFix();
+            fix.header.stamp = this->get_clock()->now();
 
-        fix.latitude = gpgga->latitude;
-        fix.longitude = gpgga->longitude;
-        fix.altitude = gpgga->altitude;
+            fix.latitude = gpgga->latitude;
+            fix.longitude = gpgga->longitude;
+            fix.altitude = gpgga->altitude;
 
-        publisher_->publish(fix);
+            publisher_->publish(fix);
+        }
+
+        // Libérer la mémoire allouée par nmea_parse
+        nmea_free(data);
     }
 
     // Libérer la mémoire allouée par nmea_parse
     nmea_free(data);
 }
 
-    rclcpp::Subscription<nmea_msgs::msg::Sentence>::SharedPtr subscription_;
-    rclcpp::Publisher<sensor_msgs::msg::NavSatFix>::SharedPtr publisher_;
-};
+rclcpp::Subscription<nmea_msgs::msg::Sentence>::SharedPtr subscription_;
+rclcpp::Publisher<sensor_msgs::msg::NavSatFix>::SharedPtr publisher_;
+}
+;
 
 int main(int argc, char **argv)
 {
