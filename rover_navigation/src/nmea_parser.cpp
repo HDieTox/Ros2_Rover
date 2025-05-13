37d6401984eddbfcbcadc5318aed73d481566fca
HDieTox <tom.compagnon2003@gmail.com>
2025-05-13 15:35:25 +0200

    +

--- rover_navigation/src/nmea_parser.cpp
+++ rover_navigation/src/nmea_parser.cpp
@@ -22,9 +22,9 @@ public:
 private:
   void nmea_callback(const std_msgs::msg::String::SharedPtr msg)
   {
-    if (msg->data.compare(3, 5, "GNGGA") == 0)
+    if (msg->data.compare(1, 5, "GNGGA") == 0)
     {
-      msg->data.replace(3, 5, "GPGGA");
+      msg->data.replace(1, 5, "GPGGA");
     }
 
     const std::string &str = msg->data;
